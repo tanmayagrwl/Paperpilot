520a2fd4ff0f0261c9396f7ac673c14085e1d1a3
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-10 19:39:07 +0530

    integrate logo font file

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../Assets/logo.png'
 
 function Landing() {
   return (
@@ -8,8 +9,13 @@ function Landing() {
             <div className="absolute inset-0 bg-gradient-to-r from-[#1E1E1E] to-[#2C2A30] transition-all duration-1000
                 flex justify-between">
 
-                <div>
-                    
+                <div className='flex flex-col justify-around'>
+                    <div className='font-racing display flex text-5xl text-[#D4D4D4]'>
+                        <img src={Logo} alt="logo" />
+                        Paperpilot
+                    </div>
+                    <div></div>
+                    <div></div>
                 </div>
 
 
@@ -22,7 +28,7 @@ function Landing() {
 
             </div>
         </div>
-
+        <div className='relative bg-[#8257E5] h-[2vh]'></div>
 
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        racing: ["RACING","sans-serif"] 
+      }
+    },
   },
   plugins: [],
 }
