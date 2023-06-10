4574af4b2b7e884e59eb089ed071e7167e6e8ba5
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-10 16:03:38 +0530

    add bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Landing from './Components/Landing'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-900">
-      Hello world!
-    </h1> 
+      <Landing />
     </>
   )
 }
---
+++ src/Components/Landing.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Landing() {
+  return (
+    <div className=''>
+        <div className="relative bg-gray-900 h-[98vh]">
+
+            <div className="absolute inset-0 bg-gradient-to-r from-[#1E1E1E] to-[#2C2A30] transition-all duration-1000
+                flex justify-between">
+
+                <div>
+                    
+                </div>
+
+
+                <div>
+
+                </div>
+
+
+
+
+            </div>
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Landing
