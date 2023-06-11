218af5f63d6a0ff70c9dc88827586ed13c091ac1
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-11 12:48:56 +0530

    build responsive left half

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -6,25 +6,23 @@ function Landing() {
     <div className=''>
         <div className="relative bg-gray-900 h-[98vh]">
 
-            <div className="absolute inset-0 bg-gradient-to-r from-[#1E1E1E] to-[#2C2A30] transition-all duration-1000
-                flex justify-between">
-
-                <div className='flex flex-col justify-around'>
-                    <div className='font-racing display flex text-5xl text-[#D4D4D4]'>
-                        <img src={Logo} alt="logo" />
-                        Paperpilot
+            <div className="absolute inset-0 bg-gradient-to-r from-black to-[#2C2A30] transition-all duration-1000">
+               
+                <div className='h-[98vh] flex justify-between ml-5 sm:ml-5 md:ml-20'>
+                    <div className='flex flex-col justify-around mt-10'>
+                        <div className='font-racing display flex text-5xl text-[#D4D4D4]'>
+                            <img src={Logo} alt="logo" className='pr-4' />
+                            Paperpilot
+                        </div>
+                        <div className='font-roboto_bold text-white text-7xl'>Get your research papers <br /> on Autopilot.</div>
+                        <img src={Tag} alt="tag" className='w-[80%] sm:w-[80%] md:w-[45%] lg:w-[55%]'  />
                     </div>
-                    <div className='font-roboto_bold text-white'>Get your research papers on Autopilot.</div>
-                    <div className=''><img src={Tag} alt="tag" /></div>
-                </div>
 
 
-                <div>
-
-                </div>
+                    <div></div>
 
 
-
+                </div>    
 
             </div>
         </div>
