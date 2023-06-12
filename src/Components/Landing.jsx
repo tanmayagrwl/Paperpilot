42af2641c190c10efee0029958a45f41effda3a6
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-12 10:50:22 +0530

    align code

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -6,22 +6,28 @@ function Landing() {
     /* parent div */
     <div className=''>
         {/*transition div  to-[#2C2A30] */}
-       <div className="bg-gradient-to-r from-black to-[#2C2A30] transition-colors duration-500 h-[98vh] w-screen">
+       <div className="bg-gradient-to-r from-black to-[#2C2A30] transition-colors duration-500 h-[100vh] sm:h-[100vh] md:h-[98vh] w-screen">
+
             {/* grid formation*/}
-            <div  className='grid grid-cols-2 grid-rows-1'>
+            <div  className='grid grid-cols-1 grid-rows-2 sm:grid-cols-1 sm:grid-rows-2 md:grid-cols-2 md:grid-rows-1'>
+
                 {/* grid 1 */}
                 <div className='h-[100vh] w-[100%] pl-10'>
+
                     {/* creating flex */}
                     <div className='flex h-full w-full flex-col justify-around'>
+
                         {/* adding logo */}
                         <div className='font-racing display flex text-5xl text-[#D4D4D4]'>
                             <img src={Logo} alt="logo" className='pr-4' />
                             Paperpilot
                         </div>
-                        <div className='font-roboto_bold text-white text-7xl'>Get your research papers <br /> on Autopilot.</div>
-                        <img src={Tag} alt="tag" className='w-[80%] sm:w-[80%] md:w-[45%] lg:w-[55%]'  />
 
-                    
+                        {/* add tagline */}
+                        <div className='font-roboto_bold text-white text-7xl'>Get your research papers <br /> on Autopilot.</div>
+
+                        {/* add creator img */}
+                        <img src={Tag} alt="tag" className='w-[80%] sm:w-[80%] md:w-[45%] lg:w-[55%]'  />
 
                     </div>
                 </div>
