3cfa41ee6016b17c8e3c7f6a587ec10d5aba04e6
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-12 10:13:57 +0530

    add grid

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -6,9 +6,10 @@ function Landing() {
     /* parent div */
     <div className=''>
         {/*transition div  to-[#2C2A30] */}
-       <div class="bg-gradient-to-r from-black to-[#2C2A30] transition-colors duration-500 h-[98vh] w-screen">
-            <div>
-                
+       <div className="bg-gradient-to-r from-black to-[#2C2A30] transition-colors duration-500 h-[98vh] w-screen">
+            <div  className='grid grid-cols-2 grid-rows-1'>
+                <div className='h-[100vh] w-[100%] bg-red-700'></div>
+                <div className='h-[100vh] w-[100%] bg-pink-700'></div>
             </div>
 
 
