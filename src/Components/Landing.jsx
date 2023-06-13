48b2362033f883c7a8e25b95b3c17869ebd09410
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-13 11:25:07 +0530

    make landing template responsive in sm

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -18,13 +18,13 @@ function Landing() {
                     <div className='flex h-full w-full flex-col justify-around'>
 
                         {/* adding logo */}
-                        <div className='font-racing display flex text-5xl text-[#D4D4D4]'>
+                        <div className='font-racing display flex text-3xl sm:text-3xl lg:text-5xl text-[#D4D4D4]'>
                             <img src={Logo} alt="logo" className='pr-4' />
                             Paperpilot
                         </div>
 
                         {/* add tagline */}
-                        <div className='font-roboto_bold text-white text-7xl'>Get your research papers <br /> on Autopilot.</div>
+                        <div className='font-roboto_bold text-white text-6xl sm:text-6xl lg:text-7xl'>Get your research papers <br /> on Autopilot.</div>
 
                         {/* add creator img */}
                         <img src={Tag} alt="tag" className='w-[80%] sm:w-[80%] md:w-[45%] lg:w-[55%]'  />
@@ -33,7 +33,7 @@ function Landing() {
                 </div>
 
                 {/* grid 2 */}
-                <div className='h-[100vh] w-[100%]'>
+                <div className='h-[100vh] w-[100%] bg-gradient-to-r from-black to-[#2C2A30] transition-colors duration-500 md:bg-none'>
 
                     {/* creating flex */}
                     <div className='flex h-full w-full  justify-end items-center'>
@@ -42,13 +42,13 @@ function Landing() {
                         <div className='h-[70vh] w-[20vw] bg-[#633BBC] z-10 absolute rounded-l-3xl'></div>
 
                         {/* box2 */}
-                        <div className='h-[45vh] w-[10vw] bg-[#714CC2] z-20 mr-9 absolute rounded-r-lg'></div>
+                        <div className='h-[45vh] w-[10vw] bg-[#714CC2] z-20 mr-4 sm:mr-4 lg:mr-9 absolute rounded-r-lg'></div>
 
                         {/* box3 */}
-                        <div className='h-[50vh] w-[20vw] bg-[#AC97DB] z-30 mr-14 absolute rounded-r-lg'></div>
+                        <div className='h-[50vh] w-[20vw] bg-[#AC97DB] z-30 mr-9 sm:mr-9 lg:mr-14 absolute rounded-r-lg'></div>
 
                         {/* box main */}
-                        <div className='h-[60vh] w-[30vw] bg-white z-40 mr-20 absolute rounded-md'>
+                        <div className='h-[60vh] w-[70vw] sm:w-[70vw] lg:w-[30vw] bg-white z-40 mr-14 sm:mr-14 lg:mr-20 absolute rounded-lg'>
 
                         </div>
 
