fdc57f9abe8cbf52310bf8333ad9d1049e5f7a1c
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-14 10:28:37 +0530

    align code

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -11,6 +11,7 @@ function Login() {
         <div className='font-roboto_bold underline text-2xl mb-8'>
             Sign In
         </div>
+        
         <Link to="/select">
             <button className="bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB] mb-5">
                 <img src={google} alt="Google logo"  class="inline-block mr-2 h-5 w-5" />
@@ -38,16 +39,6 @@ function Login() {
                 }}
             />
         </GoogleOAuthProvider>
-
-
-
-
-
-
-
-
-
-
 
 
 
