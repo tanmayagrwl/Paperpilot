8933511fba92ae85956cf49a5f7b6f8259ec8784
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-17 23:15:30 +0530

    add buttons to div responsive

--- src/Components/Btn.jsx
+++ src/Components/Btn.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Btn(props) {
   return (
     <div>
-        <button className=" flex justify-around bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB] mb-5">
+        <button className=" flex justify-around bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-48 sm:w-48 lg:w-72 mb-5">
                 <span className="inline-block">{props.name}</span>
       </button>
     </div>
--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -8,17 +8,28 @@ import Btn from './Btn'
 function Select(props) {
   const { button } = props;
   return (
-    <form action="" className='flex flex-col h-[100%] w-[100%] justify-center items-center'>
+    <form action="" className='flex flex-col h-[100%] w-[100%] justify-around items-center p-5'>
     
       
       <div className='font-roboto_bold text-2xl pb-8'>
         I want to read about...
       </div>
       
+
+
+
+    <div className=' flex flex-col items-center w-[100%] h-[50%] overflow-y-scroll mb-10'>
+
       {props.button.map((el) =>{
          return <Btn name = {el.name} />; 
       
     })}
+  </div>
+
+
+
+
+
 
         <Link to="/final">
           <button className='h-10 w-52 text-xl font-roboto bg-[#714CC2] hover:bg-[#8257E5] cursor-pointer rounded-md xl border border-x-1 border-y-1 border-solid border-[#16161d] shadow-xl text-white shadow-[#714CC2]'>
