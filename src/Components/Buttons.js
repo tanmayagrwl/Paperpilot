360e3e232d67558d142c7eb76c2c626280eccc0e
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-17 10:29:19 +0530

    create button.js json

---
+++ src/Components/Buttons.js
@@ -0,0 +1,41 @@
+button = [
+    {
+        name: "Security and Privacy"
+    },
+    {
+        name: "Robotics"
+    },
+    {
+        name: "Mathematics"
+    },
+    {
+        name: "Physics"
+    }, {
+        name: "Chemistry"
+    }, {
+        name: "Biology"
+    }, {
+        name: "Computer Science"
+    }, {
+        name: "Machine Learning"
+    }, {
+        name: "Artificial Intelligence"
+    }, {
+        name: "Quantum"
+    }, {
+        name: "Electrical"
+    }, {
+        name: "Mechanical"
+    }, {
+        name: "Civil"
+    }, {
+        name: "Arts"
+    }, {
+        name: "Design"
+    }, {
+        name: "Architecture"
+    }, {
+        name: "Power"
+    },
+
+]
--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+
+
+
+
+
 function Select() {
   return (
     <form action="" className='flex flex-col h-[100%] w-[100%] justify-center items-center'>
