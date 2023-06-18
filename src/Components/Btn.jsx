3614c7f57a9b297999616cea6e7b535d9071e4ad
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-18 16:23:20 +0530

    setup code for onclick

--- src/Components/Btn.jsx
+++ src/Components/Btn.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 
 function Btn(props) {
 
-
+  const [bgColor, setBgColor] = useState('bg-[#FFFDD0]');
 
 
   const handleClick = (event) =>{
     event.preventDefault();
+    setBgColor('bg-red-500');
+
+
   }
-    // Your custom code here
+
   
+
   return (
     <div>
-        <button className={` flex justify-around bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-48 sm:w-48 lg:w-72 mb-5  `} onClick={handleClick}>
+        <button className={` flex justify-around ${bgColor} hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-48 sm:w-48 lg:w-72 mb-5  `} onClick={handleClick}>
                 <span className="inline-block" >{props.name}</span>
       </button>
     </div>
