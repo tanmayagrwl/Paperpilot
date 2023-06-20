d75a6fb19ae5c4735eb6a2054b1273b242700be3
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-20 15:45:26 +0530

    style landing

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -4,7 +4,7 @@ import github from "../Assets/github.png";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from 'react-router-dom';
-
+import logo from '../Assets/Logo.png'
 
 
 
@@ -16,7 +16,7 @@ function Login() {
   if (isLoading) {
     return (
       <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
-        <div className="font-roboto_bold underline text-2xl mb-8">
+        <div className="font-roboto_bold underline text-2xl mb-8 text-center">
           Loading ...
         </div>
       </div>
@@ -26,15 +26,17 @@ function Login() {
   if (!isAuthenticated) {
     return (
       <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
-        <div className="font-bold text-2xl mb-8 text-[#714CC2]">
-          Please Login to proceed
+        <div className="font-bold text-2xl mb-8 text-black font-roboto_bold text-center ">
+          Welcome to PaperPilot!
         </div>
         <button
           onClick={() => loginWithRedirect()}
           type="button"
-          class="text-white bg-[#714CC2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-[#714CC2] dark:hover:bg-[#AC97DB] focus:outline-none dark:focus:ring-blue-800"
+          className="bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB] mb-5"
         >
-          Login
+          <img src={logo} alt="logo"  class="inline-block mr-2 h-5 w-5" />
+                <span className="inline-block"> Login/Register here</span>
+
         </button>
       </div>
     );
