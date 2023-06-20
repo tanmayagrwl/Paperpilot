db66167bd3304e7d6c62feace8c37d3aca32e2f9
zaid <zaid.tab123@gmail.com>
2023-06-20 12:42:13 +0530

    ⏪ uncomment predict function

--- Backend/src/api/predict/controller.ts
+++ Backend/src/api/predict/controller.ts
@@ -1,20 +1,20 @@
-// const PredictPapers = async (interests:any, paperRecommendations:any) => {
-// 	const filteredData: any = {};
-// 	const randomData: { [key: string]: { title: string; id: string } } = {};
-// 	for (const key of interests) {
-// 		if (paperRecommendations.hasOwnProperty(key)) {
-// 			filteredData[key] = paperRecommendations[key];
+const PredictPapers = async (interests: any, paperRecommendations: any) => {
+  const filteredData: any = {};
+  const randomData: { [key: string]: { title: string; id: string } } = {};
+  for (const key of interests) {
+    if (paperRecommendations.hasOwnProperty(key)) {
+      filteredData[key] = paperRecommendations[key];
 
-// 			// Randomly select a title and ID from the filtered data
-// 			const titles = filteredData[key][0];
-// 			const ids = filteredData[key][1];
-// 			const randomIndex = Math.floor(Math.random() * titles.length);
-// 			const randomTitle = titles[randomIndex];
-// 			const randomId = ids[randomIndex];
+      // Randomly select a title and ID from the filtered data
+      const titles = filteredData[key][0];
+      const ids = filteredData[key][1];
+      const randomIndex = Math.floor(Math.random() * titles.length);
+      const randomTitle = titles[randomIndex];
+      const randomId = ids[randomIndex];
 
-// 			randomData[key] = { title: randomTitle, id: randomId };
-// 		}
-// 	}
-// }
+      randomData[key] = { title: randomTitle, id: randomId };
+    }
+  }
+};
 
-// export default PredictPapers;
+export default PredictPapers;
--- Backend/src/api/predict/router.ts
+++ Backend/src/api/predict/router.ts
@@ -1,11 +1,14 @@
 import { Request, Response, Router } from "express";
-// import PredictPapers from "./controller";
+import PredictPapers from "./controller";
 
 const predictRouter = Router();
 
-const handlePredict = async(req:Request, res:Response) => {
-    // const randomData = await PredictPapers(req.body.interests, req.body.paperRecommendations);
-    res.status(200).json({success: true, data: "hello"});
+const handlePredict = async (req: Request, res: Response) => {
+  const randomData = await PredictPapers(
+    req.body.interests,
+    req.body.paperRecommendations
+  );
+  res.status(200).json({ success: true, data: "hello" });
 };
 
 predictRouter.post("/", handlePredict);
