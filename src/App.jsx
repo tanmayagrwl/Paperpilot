d573a0dceab28d77f82ed7c39fe03f2998634f24
zaid <zaid.tab123@gmail.com>
2023-06-20 12:39:17 +0530

    ✨ implement authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-
-import './App.css'
-import Landing from './Components/Landing'
-import Error from './Components/Error'; 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import "./App.css";
+import Landing from "./Components/Landing";
+import Error from "./Components/Error";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { createAuth0Client } from "@auth0/auth0-spa-js";
 function App() {
-  
-
   return (
     <>
-      <Landing/>
+      <Landing />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,35 +1,47 @@
-import React from 'react'
-import google from '../Assets/google.png'
-import github from '../Assets/github.png'
-import { Link } from 'react-router-dom'
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google'
-
+import React from "react";
+import google from "../Assets/google.png";
+import github from "../Assets/github.png";
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function Login() {
-  return (
-    <div className='w-[100%] h-[100%] flex flex-col items-center justify-center'>
-        <div className='font-roboto_bold underline text-2xl mb-8'>
-            Sign In
+  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
+
+  if (isLoading) {
+    return (
+      <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
+        <div className="font-roboto_bold underline text-2xl mb-8">
+          Loading ...
         </div>
-        
-        <Link to="/select">
-            <button className="bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB] mb-5">
-                <img src={google} alt="Google logo"  class="inline-block mr-2 h-5 w-5" />
-                <span className="inline-block"> Sign In with Google</span>
-            </button>
-        </Link>
+      </div>
+    );
+  }
 
+  if (!isAuthenticated) {
+    return (
+      <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
+        <div className="font-bold text-2xl mb-8 text-[#714CC2]">
+          Please Login to proceed
+        </div>
+        <button
+          onClick={() => loginWithRedirect()}
+          type="button"
+          class="text-white bg-[#714CC2] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-[#714CC2] dark:hover:bg-[#AC97DB] focus:outline-none dark:focus:ring-blue-800"
+        >
+          Login
+        </button>
+      </div>
+    );
+  }
 
+  return (
+    <div className="w-[100%] h-[100%] flex flex-col items-center justify-center">
+      <div className="font-bold text-2xl mb-8 text-[#714CC2]">
+        Welcome {user.name}
+      </div>
 
-        <Link to="/select">
-            <button className="bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB]">
-                <img src={github} alt="Google logo"  class="inline-block mr-2 h-5 w-5" />
-                <span className="inline-block"> Sign In with GitHub</span>
-            </button>
-        </Link>
-
-
-        {/* <GoogleOAuthProvider clientId="530224716772-ipb1litagv5le3m3jjr0hojkbhebqf10.apps.googleusercontent.com">
+      {/* <GoogleOAuthProvider clientId="530224716772-ipb1litagv5le3m3jjr0hojkbhebqf10.apps.googleusercontent.com">
             <GoogleLogin
                 onSuccess={credentialResponse => {
                     console.log(credentialResponse)
@@ -39,11 +51,8 @@ function Login() {
                 }}
             />
         </GoogleOAuthProvider> */}
-
-
-
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
