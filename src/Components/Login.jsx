ace734cf7e32039a3c859b678eacb5010f79cfb6
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-20 15:46:30 +0530

    correction

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -35,7 +35,7 @@ function Login() {
           className="bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB] mb-5"
         >
           <img src={logo} alt="logo"  class="inline-block mr-2 h-5 w-5" />
-                <span className="inline-block"> Login/Register here</span>
+                <span className="inline-block"> Login here to continue</span>
 
         </button>
       </div>
