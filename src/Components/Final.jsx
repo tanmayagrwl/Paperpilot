283088ede8ad76eeb53f347bc2f2f9a4b16d4019
tanmayagrwl <tanmayhp78@gmail.com>
2023-06-22 12:56:56 +0530

    update final.jsx

--- src/Components/Final.jsx
+++ src/Components/Final.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import ClipLoader from "react-spinners/CircleLoader";
+import github from '../Assets/github.png'
+
 
 function Final() {
 
@@ -29,11 +31,14 @@ function Final() {
          <p className='flex w-[100%] justify-center items-center text-center font-roboto_bold text-3xl pb-12'>
             Thank you for subscribing
         </p>
-        <button className='h-14 w-56 text-xl font-roboto bg-[#714CC2] hover:bg-[#8257E5] rounded-full xl border border-x-4 border-y-4 border-solid border-[#16161d] shadow-lg shadow-[#714CC2] text-[#FFFDD0]'>
-            Support more research
-        </button>
+
+        <a href="https://github.com/tanmayagrwl/Paperpilot" target="_blank" >
+        <button className="bg-[#FFFDD0] hover:bg-[#AC97DB] text-black font-bold py-2 px-4 rounded-full border-[1px] border-black focus:outline-none focus:shadow-outline w-64 shadow-xl shadow-[#AC97DB]">
+                <img src={github} alt="github logo"  class="inline-block mr-2 h-5 w-5" />
+                <span className="inline-block">Contribute on GitHub</span>
+            </button></a>
         <span className='text-xs px-10 pt-7'>
-        <span className='text-red-700'>*</span>All of your contributions matter! Every time you support our cause, you are helping to fund the latest research and make a difference in the world. Thank you for your support!
+        <span className='text-red-700'>*</span>Are you interested in contributing to an open source project or reviewing its code? Click on the link above to access the codebase.
         </span>
         </>
       }
