3f3403e98511f1136fda01bcf4e2a7abb8fd1be7
Ash <01tripster@gmail.com>
2023-06-23 10:53:41 +0530

    feat: add ses mailing service

--- Backend/src/api/predict/controller.ts
+++ Backend/src/api/predict/controller.ts
@@ -1,32 +1,37 @@
 import fs from "fs";
 
 interface JsonData {
-  [key: string]: string[][];
+	[key: string]: string[][];
 }
 
 const paperRecommendationsjson = fs.readFileSync(
-  "./src/shared/recommendations.json",
-  "utf-8"
+	"./src/shared/recommendations.json",
+	"utf-8"
 );
 const paperRecommendations: JsonData = JSON.parse(paperRecommendationsjson);
 
-const PredictPapers = async (interests: any) => {
-  const filteredData: any = {};
-  const randomData: { [key: string]: { title: string; id: string } } = {};
-  for (const key of interests) {
-    if (paperRecommendations.hasOwnProperty(key)) {
-      filteredData[key] = paperRecommendations[key];
+const PredictPapers = async (interests: string[]) => {
+	const filteredData: any = {};
+	const randomData: {
+		[key: string]: {
+			[x: string]: string;
+		};
+	} = {};
+	for (const key of interests) {
+		if (paperRecommendations.hasOwnProperty(key)) {
+			filteredData[key] = paperRecommendations[key];
 
-      // Randomly select a title and ID from the filtered data
-      const titles = filteredData[key][0];
-      const ids = filteredData[key][1];
-      const randomIndex = Math.floor(Math.random() * titles.length);
-      const randomTitle = titles[randomIndex];
-      const randomId = ids[randomIndex];
+			// Randomly select a title and ID from the filtered data
+			const titles = filteredData[key][0];
+			const ids = filteredData[key][1];
+			const randomIndex = Math.floor(Math.random() * titles.length);
+			const randomTitle = titles[randomIndex];
+			const randomId = ids[randomIndex];
 
-      randomData[key] = { title: randomTitle, id: randomId };
-    }
-  }
+			randomData[key] = { title: randomTitle, id: randomId };
+		}
+	}
+	return randomData;
 };
 
 export default PredictPapers;
--- Backend/src/api/predict/router.ts
+++ Backend/src/api/predict/router.ts
@@ -1,11 +1,16 @@
 import { Request, Response, Router } from "express";
 import PredictPapers from "./controller";
+const sendRandomTitleEmail = require("../../shared/ses");
 
 const predictRouter = Router();
 
 const handlePredict = async (req: Request, res: Response) => {
-  const randomData = await PredictPapers(req.body.interests);
-  res.status(200).json({ success: true, data: randomData });
+	const randomData = await PredictPapers(req.body.interests);
+
+	//store keys of randomData in an array
+	const keys = Object.keys(randomData);
+	await sendRandomTitleEmail(req.body.email, keys[0], keys[1], keys[2]);
+	res.status(200).json({ success: true, data: randomData });
 };
 
 predictRouter.post("/", handlePredict);
