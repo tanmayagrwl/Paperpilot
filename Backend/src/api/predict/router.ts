ed674e498b5c51a05f831b5852ec70e4b1650989
zaid <zaid.tab123@gmail.com>
2023-06-23 16:42:20 +0530

    ✨ email works wohoo

--- Backend/src/api/predict/router.ts
+++ Backend/src/api/predict/router.ts
@@ -1,16 +1,16 @@
 import { Request, Response, Router } from "express";
-import PredictPapers from "./controller";
-const sendRandomTitleEmail = require("../../shared/ses");
+import PredictPapers, { sendPaperToEmail } from "./controller";
 
 const predictRouter = Router();
 
 const handlePredict = async (req: Request, res: Response) => {
-	const randomData = await PredictPapers(req.body.interests);
+  const randomData = await PredictPapers(req.body.interests);
 
-	//store keys of randomData in an array
-	const keys = Object.keys(randomData);
-	await sendRandomTitleEmail(req.body.email, keys[0], keys[1], keys[2]);
-	res.status(200).json({ success: true, data: randomData });
+  //store keys of randomData in an array
+  const keys = Object.keys(randomData);
+  console.log("keys", keys);
+  await sendPaperToEmail(req.body.email, keys);
+  res.status(200).json({ success: true, data: randomData });
 };
 
 predictRouter.post("/", handlePredict);
--- Backend/src/config/index.ts
+++ Backend/src/config/index.ts
@@ -16,8 +16,8 @@ export default {
   },
 
   awsConfig: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     region: process.env.AWS_REGION,
   },
 
