7ca1906d48ff75196c1341183631363781f600db
zaid <zaid.tab123@gmail.com>
2023-06-24 15:36:48 +0530

    🐛 minor fix

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -19,7 +19,7 @@ function Select(props) {
       interests: selectedButtonNames,
     };
 
-    fetch(`${import.meta.env.VITE_API_ENDPOINT}/api/predict`, {
+    fetch(`${import.meta.env.VITE_API_ENDPOINT}api/predict`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
